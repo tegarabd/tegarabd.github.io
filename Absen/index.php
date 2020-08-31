5e1f8a1e49dc5b82385ffce6a7fbbe9faa30529b
tegarabd <70501690+tegarabd@users.noreply.github.com>
2020-09-01 06:22:36 +0700

    Add files via upload

---
+++ Absen/index.php
@@ -0,0 +1,76 @@
+<?php
+	$date=date('d m Y');
+?>
+
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+	<title>Absensi</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/reset.css">
+</head>
+<body>
+	<div class="container">
+		<header>
+			<div class="kemenag">
+				<img src="img/kemenag.png" alt="">
+			</div>
+			<div class="madan">
+				<img src="img/madan.png" alt="">
+			</div>
+			<div class="judul">
+				<h1>LAPORAN</h1>
+				<h1>ABSENSI</h1>
+			</div>
+		</header>
+		<main>
+			<div class="guru">
+				<img src="img/rabu.jpg" alt="">
+			</div>
+			<div class="date">
+				<h2>XII MIA 4</h2>
+				<h3><?php echo $date; ?></h3>
+			</div>
+		</main>
+		<footer>
+			<img src="img/1.jpeg" alt="">
+			<img src="img/2.jpeg" alt="">
+			<img src="img/3.jpeg" alt="">
+			<img src="img/4.jpeg" alt="">
+			<img src="img/5.jpeg" alt="">
+			<img src="img/6.jpeg" alt="">
+			<img src="img/7.jpeg" alt="">
+			<img src="img/8.jpeg" alt="">
+			<img src="img/9.jpeg" alt="">
+			<img src="img/10.jpeg" alt="">
+			<img src="img/11.jpeg" alt="">
+			<img src="img/12.jpeg" alt="">
+			<img src="img/13.jpeg" alt="">
+			<img src="img/14.jpeg" alt="">
+			<img src="img/15.jpeg" alt="">
+			<img src="img/16.jpeg" alt="">
+			<img src="img/17.jpeg" alt="">
+			<img src="img/18.jpeg" alt="">
+			<img src="img/19.jpeg" alt="">
+			<img src="img/20.jpeg" alt="">
+			<img src="img/21.jpeg" alt="">
+			<img src="img/22.jpeg" alt="">
+			<img src="img/23.jpeg" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<p></p>
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+		</footer>
+	</div>
+</body>
+</html>
